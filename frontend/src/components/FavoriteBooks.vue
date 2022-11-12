029877d388d76b4af68400850946a10bee65d9cd
Zhigang Wang <zhigangw@hotmail.com>
2022-11-12 15:47:45 -0800

    finishing favorite books

--- frontend/src/components/FavoriteBooks.vue
+++ frontend/src/components/FavoriteBooks.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        <p>Favorite Books</p>
+    <div class="booksContainer">
+        <p class="booksTitle">Favorite Books</p>
+        <div class="booksBox">
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+            <img class="book" src="../assets/images/books/Book1.jpg" />
+        </div>
     </div>
 </template>
 <script>
@@ -14,5 +23,65 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.booksContainer {
+    width: 100%;
+    @extend .d-flex;
+    @extend .flex-column;
+    @extend .justify-content-center;
 
+    padding: 68px 88px;
+    gap: 32px;
+
+    /* White */
+
+    background: #FFFFFF;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 3;
+    align-self: stretch;
+    flex-grow: 0;
+}
+
+.booksTitle {
+    /* Title2 | Black */
+
+    font-family: 'Nunito';
+    font-style: normal;
+    font-weight: 900;
+    font-size: 36px;
+    line-height: 49px;
+
+    /* Dark Blue */
+
+    color: #2B70C9;
+
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+}
+
+.booksBox {
+    @extend .d-flex;
+    @extend .justify-content-between;
+    @extend .mx-auto;
+    width: 90%;
+}
+
+.book {
+    width: 10%;
+
+    filter: drop-shadow(2px 2px 23px rgba(0, 0, 0, 0.1));
+    border-radius: 8px;
+
+    /* Inside auto layout */
+
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+}
 </style>
